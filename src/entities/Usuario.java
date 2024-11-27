13177f446594a03373d778239cc1230da27e3e39
JoaoVSMonteiro <joao_dj58@hotmail.com>
2024-11-27 16:09:39 -0300

    subindo classe usuarios

--- src/entities/Usuario.java
+++ src/entities/Usuario.java
@@ -1,4 +1,29 @@
 package entities;
 
+import java.util.ArrayList;
+
 public class Usuario {
+    private String nome;
+    private int id;
+    private ArrayList<String> livrosEmprestados;
+
+    public Usuario(String nome, int id) {
+        this.nome = nome;
+        this.id = id;
+        this.livrosEmprestados = new ArrayList<>();
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public ArrayList<String> getLivrosEmprestados() {
+        return livrosEmprestados;
+    }
+
 }
+
